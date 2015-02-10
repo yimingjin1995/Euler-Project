b8ffddd3c8773b4d5059b40ec30a1f30404c2d8d
yimingjin1995 <yimingjin1995@gmail.com>
2015-02-10 01:55:09 -0600

    question1
    netid: yjin34

---
+++ question1.js
@@ -0,0 +1,11 @@
+var sum = 0;
+for (i3 = 0; i3 < 1000; i3 = i3 + 3){
+	sum = sum + i3;
+}
+for (i5 = 0; i5 < 1000; i5 = i5 + 5){
+	sum = sum + i5;
+}
+for (i15 = 0; i15 < 1000; i15 = i15 + 15){
+	sum = sum - i15;
+}
+
